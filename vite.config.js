9b48ce8ae9b9c389fc410f7ae14b88f855bec3a7
Soad-Alzain <soadalzain640@gmail.com>
2026-08-31 09:26:40 +0300

    Configure vite.config.ts for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/madad-blood-bank/',
   plugins: [
     vue(),
     vueDevTools(),
